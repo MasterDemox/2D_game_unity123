6bd4db792feee17bed232a82ad63457e79f9e8f6
Tihon06 <kotp2000@bk.ru>
2024-11-21 19:11:16 +0500

    добавил счётчик убийств

--- Assets/enemyrecords/EnemyKillTracker.cs
+++ Assets/enemyrecords/EnemyKillTracker.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 public class EnemyKillTracker : MonoBehaviour
 {
     private int killCount = 0; // Счетчик убитых врагов
 
+    public GameObject UIText;
+    
+        
+    
+
+   
     // Метод для увеличения счетчика убитых врагов
     public void IncrementKillCount()
     {
         killCount++;
         Debug.Log($"Total enemies killed: {killCount}");
+        UIText.GetComponent<Text>().text = "Убито врагов:" + killCount.ToString();
     }
 }
